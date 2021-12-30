3959df7bcebff8967aea85fda79d0d5147022742
Junaid Khan <rjmk159@gmail.com>
2021-12-30 14:35:20 +0530

    Delete Desktop/_icpa/Icpa directory

--- Desktop/_icpa/Icpa/src/components/Loader.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ActivityIndicator} from "react-native";
-import './style.scss';
-
-export function Loader() {
-  return   <ActivityIndicator size="large" />
-}
-export default Loader;
--- Desktop/_icpa/Icpa/src/constants/index.js
+++
@@ -1,9 +0,0 @@
-import icpaTheme from './Theme';
-import Images from './Images';
-import tabs from './tabs';
-
-export {
-  icpaTheme,
-  Images,
-  tabs
-};
